3b783bea849cd76cb168a30584d89befdd076b26
smilemari123 <91219666+smilemari123@users.noreply.github.com>
2021-10-08 11:00:29 +0300

    eng version

--- simple.c
+++ simple.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
     char c, m[64]={};
     int i = 0, fl = 0;
-    printf ("Введите число с плавающей точкой: ");
-    while (((c=getchar())!='\n') && (i < 64)){
+    printf ("Enter a number: ");
+    while (((c = getchar()) != '\n') && (i < 64)){
         if ((c != '.') && ((c > '9') || (c < '0'))){
-            printf ("Введен неверный символ"); 
+            printf ("Invalid character entered"); 
             exit (1);
         }
         if (c == '.'){
             fl = 1; 
             c = '|';
         }
-        m[i++]=c;
+        m[i++] = c;
     }
     
     if (fl == 0){
-        printf ("Введенное число не содержит точки или содержит больше 64 символов"); 
+        printf ("The entered number does not contain a dot or contains more than 64 characters"); 
         exit (1);
     }
     
     i = 0;
     while ((c = m[i]) != '\0')
         printf ("%c", m[i++]);
-
     return 0;
 }
