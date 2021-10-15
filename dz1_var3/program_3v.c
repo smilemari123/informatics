f69089089415bdfe6f7f877dba243aaee2ba7547
Maria <volynckina.masha@yandex.ru>
2021-10-15 11:44:42 +0300

    dz2

--- dz1_var3/program_3v.c
+++ dz1_var3/program_3v.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(void)
+int main()
 {
     char c, m[64]={};
     int i = 0, fl = 0, a = 0, b = 0, x =0, y = 0; 
     //a - число знаков до точки, b - после точки, x - число до точки, y - число после точки
     
-    printf ("Enter a number: ");
-    while (((c=getchar()) != '\n') && (i <= 64)){
+    printf ("Введите число с плавающей точкой: ");
+    while (((c=getchar())!='\n') && (i <= 64)){
         if ((c != '.') && ((c > '9') || (c < '0'))){
-            printf ("Invalid character entered"); 
+            printf ("Введен неверный символ"); 
             exit (1);
         }
         if (fl == 1){
@@ -27,7 +27,7 @@ int main(void)
     }
 
     if (fl == 0){
-        printf ("The entered number does not contain a dot or contains more than 64 characters"); 
+        printf ("Введенное число не содержит точки или содержит больше 64 символов"); 
         exit (1);
     }
     
@@ -35,14 +35,14 @@ int main(void)
     i = 0;
     fl = a - 1; 
     while (i < a){ 
-        x += (m[i++] - '0') * pow(10, fl--);
+        x += (m[i++] - '0')*pow(10, fl--);
     }
     
     //число после точки
     ++i;
     fl = b - 1;
     while (i < a + b + 1){
-        y += (m[i++] - '0') * pow(10, fl--);
+        y += (m[i++] - '0')*pow(10, fl--);
     }
     printf("%d | %d", x, y);
     
